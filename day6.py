9bd1d7218433862a97a50464acbb6cb6e35a75fc
Tim Wong <k1ck3rtw@gmail.com>
2018-12-12 22:39:35 -0500

    day6ish

--- day6.py
+++ day6.py
@@ -1,5 +1,25 @@
-def findManhattenDistance(x0, y0, x1, y1):
-    return abs(x0 - x1) + abs(y0 - y1)
+def findManhattenDistance(p1, p2):
+    return abs(p1[0] - p2[0]) + abs(p1[1] - p2[1])
+
+def findClosestCoordinate(currentPoint, coordinates):
+    distList = [0] * len(coordinates)
+    for i, coord in enumerate(coordinates):
+        dist = findManhattenDistance(currentPoint, coord)
+        distList[i] = (i, dist)
+    distList = sorted(distList, key=lambda x: x[1])
+
+    if distList[0][1] == distList[1][1]:
+        return -1
+
+    return distList[0][0]
+
+
+def grid_init(width, height):
+    grid = [None] * height
+    for i in range(height):
+        grid[i] = [None] * width
+
+    return grid
 
 minX = 10000
 maxX = 0
@@ -19,3 +39,14 @@ with open("./inputs/day6.txt", "r") as f:
         maxY = y if maxY < y else maxY
         coordinates.append((x, y))
 
+print("{},{}".format(maxX, maxY))
+grid = grid_init(maxX + 1, maxY + 1)
+
+for i in range(maxX):
+    for j in range(maxY):
+        currentPoint = (i, j)
+        closeCoordIdx = findClosestCoordinate(currentPoint, coordinates)
+        print("{},{}".format(i,j))
+        grid[i][j] = closeCoordIdx
+
+print(grid)
